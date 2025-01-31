0f993a76b049538579bb0cc2ef2b3b5eaacff3e9
wamo007 <shamil010@hotmail.com>
2025-01-31 13:08:00 +0100

    replace scraping logic
    add worker nodes connection

--- controllers/scrapeController.js
+++ controllers/scrapeController.js
@@ -1,46 +1,56 @@
-const fundaScraper = require('./scrapers/funda')
-const hAnywhereScraper = require('./scrapers/hAnywhere')
-const parariusScraper = require('./scrapers/pararius')
-const rentolaScraper = require('./scrapers/rentola')
-const kamernetScraper = require('./scrapers/kamernet')
-const huurwoningenScraper = require('./scrapers/huurwoningen')
-const kamerNLScraper = require('./scrapers/kamerNL')
+const axios = require('axios')
+
+const workerServers = [
+    { url: 'http://localhost:3001/extractor', scrapers: ['funda'] },
+    { url: 'http://localhost:3002/extractor', scrapers: ['hAnywhere'] },
+    { url: 'http://localhost:3003/extractor', scrapers: ['kamernet'] },
+    { url: 'http://localhost:3004/extractor', scrapers: ['kamerNL'] },
+    { url: 'http://localhost:3005/extractor', scrapers: ['pararius'] },
+    { url: 'http://localhost:3006/extractor', scrapers: ['huurwoningen'] },
+    { url: 'http://localhost:3007/extractor', scrapers: ['rentola'] },
+]
 
 const scrapeController = async (req, res) => {
-    const city = req.body.city
-    const radius = req.body.radius
-    const selectedProviders = req.body.selectedProviders
-    const sortGlobal = req.body.sortGlobal
-    const minPrice = req.body.minPrice
-    const maxPrice = req.body.maxPrice
+    const { city, radius, selectedProviders, sortGlobal, minPrice, maxPrice } = req.body
 
     if (!city) {
         return res.status(400)
         .send({ error: 'Please, provide information about the city.' })
     }
 
-    const scrapers = {
-        funda: fundaScraper,
-        hAnywhere: hAnywhereScraper,
-        kamernet: kamernetScraper,
-        kamerNL: kamerNLScraper,
-        pararius: parariusScraper,
-        huurwoningen: huurwoningenScraper,
-        rentola: rentolaScraper,
-    }
+    // console.log(`Processing the request for ${city}, ${radius} km, ${selectedProviders}, ${sortGlobal}, ${minPrice} - ${maxPrice}. Time: ${new Date()}`)
 
     res.setHeader('Content-Type', 'application/json')
 
     try {
-        for (const providerId of selectedProviders) {
-            const scraper = scrapers[providerId]
-            const data = await scraper(city, radius, sortGlobal, minPrice, maxPrice)
-            res.write(JSON.stringify({ [providerId]: data }) + '\n')
-        }
+        let completedProviders = 0
+        const totalProviders = workerServers.reduce((acc, worker) => {
+            const scrapersToRun = worker.scrapers.filter(scraper => selectedProviders.includes(scraper))
+            return acc + (scrapersToRun.length > 0 ? 1 : 0)
+        }, 0)
 
-        res.end()
-
+        workerServers.forEach(worker => {
+            const scrapersToRun = worker.scrapers.filter(scraper => selectedProviders.includes(scraper))
+            if (scrapersToRun.length > 0) {
+                axios.post(worker.url, {
+                    scrapers: scrapersToRun,
+                    city,
+                    radius,
+                    sortGlobal,
+                    minPrice,
+                    maxPrice,
+                }).then(workerResponse => {
+                    res.write(JSON.stringify(workerResponse.data) + '\n')
+                    completedProviders++
+                    if (completedProviders === totalProviders) {
+                        res.end()
+                        // console.log(`Time of completion for ${city} - ${new Date()}`)
+                    }
+                })
+            }
+        })
     } catch (error) {
+        console.error('Error finding information on the websites:', error)
         res.status(500)
         .json({ success: false, error: 'Failed to find info on the websites.' })
     }
